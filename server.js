a8d09f82a0f4c4f97192e20ecc5245b30ad526e9
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-07-13 11:00:15 -0600

    server has array

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ var PORT = 3000;
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  var customers = [{
+    customerName: $("#name").val().trim(),
+    phoneNumber: $("#phone").val().trim(),
+    customerEmail: $("#email").val().trim(),
+    customerID: $("#id").val().trim()
+
+  }
+  ];
+
+   $.post("/api/characters", customers)
+   .then(function(data) {
+     console.log("add.html", data);
+     alert("Adding character...");
+   });
